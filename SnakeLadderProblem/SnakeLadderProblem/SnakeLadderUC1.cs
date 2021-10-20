873e7b8ce7d639d71a5b0d0f87684764575d269a
Rakesh-space <rakeshsmusale03@gmail.com>
2021-10-20 23:54:30 +0530

    Snake Ladder Problem Push here

---
+++ SnakeLadderProblem/SnakeLadderProblem/SnakeLadderUC1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadderProblem
+{
+    class SnakeLadderUC1
+    {
+        public void snakeLadderUC1()
+        {
+                int Is_Present = 1;
+                Random random = new Random();
+                int empInput = random.Next(0, 2);
+                if (Is_Present == empInput)
+                {
+                    Console.WriteLine("Start the game your possition is: 0");
+                    int rollDiaValue = random.Next(1, 6);
+
+                    Console.WriteLine("Roll Dia Value is:" + rollDiaValue);
+                }
+                else
+                {
+                    Console.WriteLine(" play again");
+                }
+        }
+    }
+}
---
+++ SnakeLadderProblem/SnakeLadderProblem/SnakeLadderUC5.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadderProblem
+{
+    class SnakeLadderUC5
+    {
+        public const int snakeKillValue = 1;    //constant value to provide the snake kill
+        public const int ladderValue = 5;        //constant value to provide the ladder
+        public const int totalChartValue = 100;   //constant value to provide the chart value
+        // public const int totalNumOfPlayer = 4;      //new added here total number of player
+        public void snakeLadderUC5()
+        {
+            int countPossi = 0;
+            int totalCount = 0;
+            int countLadder = 0;
+            int snakeKill = 0;
+
+            for(int roll = 0; roll <totalChartValue; roll++)
+            {
+                Console.WriteLine("Roll Die..");
+                Random random = new Random();
+                int rollDiaValue = random.Next(1, 6);
+
+                if (rollDiaValue == snakeKillValue)
+                {
+                    snakeKill = rollDiaValue - snakeKillValue;
+                    Console.WriteLine("Snake is killed" + snakeKill);
+                }
+                else if (rollDiaValue == ladderValue)
+                {
+                    countLadder = rollDiaValue + ladderValue;
+                    Console.WriteLine("Ladder increment possition:" + countLadder);
+                }
+                else
+                {       
+                    totalCount = countPossi + rollDiaValue;
+                    Console.WriteLine("Total Count is:" + totalCount);
+                }
+            }
+            Console.WriteLine("player is win");
+        }
+    }
+}
