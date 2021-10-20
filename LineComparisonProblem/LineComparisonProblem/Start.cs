abbb89e01be929386babf3db556a5ce86845ab26
Rakesh-space <rakeshsmusale03@gmail.com>
2021-10-20 23:40:34 +0530

    Line Comparison Problem Push here

---
+++ LineComparisonProblem/LineComparisonProblem/Start.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LineComparisonProblem
+{
+    class Start
+    {
+        static void Main(string[] args)
+        {
+
+            LineComparisonUC1 lineComparisonUC1 = new LineComparisonUC1();
+            lineComparisonUC1.lineComparisonUC1();
+
+            LineComparisonUC2 lineComparisonUC2 = new LineComparisonUC2();
+            lineComparisonUC2.lineComparisonUC2();
+
+            LineComparisonUC3 lineComparisonUC3= new LineComparisonUC3();
+            lineComparisonUC3.lineComparisonUC3();
+
+            LineComparisonUC4 lineComparisonUC4=new LineComparisonUC4();
+            lineComparisonUC4.lineComparisonUC4();
+        }
+    }
+}
