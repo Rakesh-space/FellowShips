50a6237d1f42054c4eda9fcc893f640b77c4cab1
Rakesh-space <rakeshsmusale03@gmail.com>
2021-10-27 17:52:03 +0530

    StackQueueProblem Push here

---
+++ StackQueueProblem/StackQueueProblem/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace StackQueueProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("\n Stack operation :");
+            Console.WriteLine("\n UC1StackProblem.");
+            UC1StackProblem st = new UC1StackProblem();
+            st.push(56);
+            st.push(30);
+            st.push(70);
+            st.display();*/
+
+
+            /* Console.WriteLine("\n UC2StackProblem.");
+              UC2StackProblem stackProblem = new UC2StackProblem();
+              stackProblem.push(56);
+              stackProblem.push(30);
+              stackProblem.push(70);
+              stackProblem.peek();
+              stackProblem.display();
+              stackProblem.pop();
+              stackProblem.display();*/
+
+            /* Console.WriteLine("\n UC3QueueProblem :");
+             UC3QueueProblem queueProblem = new UC3QueueProblem();
+             queueProblem.Enqueue(56);
+             queueProblem.Enqueue(30);
+             queueProblem.Enqueue(70);
+             queueProblem.display();*/
+
+            /*Console.WriteLine("\n UC4QueueProblem :");
+            UC4QueueProblem queueProblem = new UC4QueueProblem();
+            queueProblem.Enqueue(56);
+            queueProblem.Enqueue(30);
+            queueProblem.Enqueue(70);
+            queueProblem.Dequeue();
+            queueProblem.display(); */
+
+            Console.ReadLine();
+        }
+    }
+}
