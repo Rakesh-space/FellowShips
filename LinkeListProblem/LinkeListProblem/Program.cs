e65244e3ee6996acddcca275a0e9c242890a6b47
Rakesh-space <rakeshsmusale03@gmail.com>
2021-10-27 17:58:47 +0530

    Linked List Problem Push here

---
+++ LinkeListProblem/LinkeListProblem/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace LinkeListProblem
+{
+    class Program
+    {
+            static void Main(string[] args)
+            {
+
+            /*    Console.WriteLine("\n UC1LinkedListProblem..");
+                UC1LinkedListProblem mylist = new UC1LinkedListProblem();
+                mylist.insert(56);
+                mylist.insert(30);
+                mylist.insert(70);
+                mylist.insert(90);
+                mylist.DisplayAllEle();*/
+
+            /*     Console.WriteLine("\n UC2LinkedListProblem..");
+                 UC2LinkedListProblem mylist2 = new UC2LinkedListProblem();
+                 Console.WriteLine("\n AddFirst Operation Performed here:");
+                 mylist2.AddFirst(56);
+                 mylist2.AddFirst(30);
+                 mylist2.AddFirst(70);
+                 mylist2.DisplayAllEle();*/
+
+            /*     Console.WriteLine("\n UC3LinkedListProblem..");
+                 UC3LinkedListProblem mylist3=new UC3LinkedListProblem();
+                 Console.WriteLine("\n AddLast Operation Performed here:");
+                 mylist3.AddLast(56);
+                 mylist3.AddLast(30);
+                 mylist3.AddLast(70);
+                 mylist3.DisplayAllEle();*/
+
+            /*   Console.WriteLine("\n UC4LinkedListProblem..");
+               UC4LinkedListProblem mylist4 = new UC4LinkedListProblem();
+               Console.WriteLine("\n insertAfter Operation Performed here:");
+               mylist4.insert(56);
+               mylist4.insert(70);
+               mylist4.insertAfter(mylist4.head.next, 30);
+               mylist4.DisplayAllEle();*/
+
+            /*Console.WriteLine("\n UC5LinkedListProblem..");
+              UC5LinkedListProblem mylist5 = new UC5LinkedListProblem();
+              Console.WriteLine("\n insertAfter Operation Performed here:");
+              mylist5.insert(56);
+              mylist5.insert(30);
+              mylist5.insert(70);
+              mylist5.RemoveFirst();
+              mylist5.DisplayAllEle();*/
+
+            /*   Console.WriteLine("\n UC6LinkedListProblem..");
+               UC6LinkedListProblem mylist6 = new UC6LinkedListProblem();
+               Console.WriteLine("\n insertAfter Operation Performed here:");
+               mylist6.insert(56);
+               mylist6.insert(30);
+               mylist6.insert(70);
+               mylist6.RemoveLast();
+               mylist6.DisplayAllEle();*/
+
+            /*  Console.WriteLine("\n UC7LinkedListProblem..");
+              UC7LinkedListProblem mylist7 = new UC7LinkedListProblem();
+              Console.WriteLine("\n insertAfter Operation Performed here:");
+              mylist7.insert(56);
+              mylist7.insert(30);
+              mylist7.insert(70);
+              mylist7.DisplayAllEle();
+              mylist7.Search(30);
+              mylist7.DisplayAllEle();*/
+
+            /* Console.WriteLine("\n UC8LinkedListProblem..");
+             UC8LinkedListProblem mylist8 = new UC8LinkedListProblem();
+             Console.WriteLine("\n insertAfter Operation Performed here:");
+             mylist8.insert(56);
+             mylist8.insert(30);
+             mylist8.insert(70);
+             mylist8.insertAfter(mylist8.head.next, 40);
+             mylist8.DisplayAllEle();*/
+
+            /*    Console.WriteLine("\n UC9LinkedListProblem..");
+                UC9LinkedListProblem mylist9 = new UC9LinkedListProblem();
+                Console.WriteLine("\n deleteAfter Operation Performed here:");
+                mylist9.insert(56);
+                mylist9.insert(30);
+                mylist9.insert(40);
+                mylist9.insert(70);
+                //   mylist9.DisplayAllEle();
+                mylist9.deleteAtPossi(3);
+                mylist9.DisplayAllEle();*/
+
+         /*   Console.WriteLine("\n UC10LinkedListProblem..");
+            UC10LinkedListProblem mylist10 = new UC10LinkedListProblem();
+            Console.WriteLine("\n Accending order Operation Performed here:");
+            mylist10.insert(56);
+            mylist10.insert(30);
+            mylist10.insert(40);
+            mylist10.insert(70);
+            mylist10.sortedInsert();
+            mylist10.DisplayAllEle();*/
+
+            Console.ReadLine();
+            }
+    }
+}
