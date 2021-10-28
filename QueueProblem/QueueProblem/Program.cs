2a21fa6334af7e4af52ec78ca1aee45115cb5ac3
Rakesh-space <rakeshsmusale03@gmail.com>
2021-10-28 14:13:27 +0530

    Updated Queue realated Code

---
+++ QueueProblem/QueueProblem/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace QueueProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Console.WriteLine("\n UC1QueueProblem :");
+           UC3QueueProblem queueProblem = new UC3QueueProblem();
+           queueProblem.Enqueue(56);
+           queueProblem.Enqueue(30);
+           queueProblem.Enqueue(70);
+           queueProblem.display();*/
+
+            /*Console.WriteLine("\n UC2QueueProblem :");
+            UC4QueueProblem queueProblem = new UC4QueueProblem();
+            queueProblem.Enqueue(56);
+            queueProblem.Enqueue(30);
+            queueProblem.Enqueue(70);
+            queueProblem.Dequeue();
+            queueProblem.display(); */
+        }
+    }
+}
