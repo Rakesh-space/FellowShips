e9bb7c0ec33cc1f24f957ad10ab424a674fbe981
Rakesh-space <rakeshsmusale03@gmail.com>
2021-10-28 14:02:16 +0530

    Updated Only Stack Realated Code

---
+++ StackProblem/StackProblem/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace StackQueueProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("\n Stack operation :");
+            Console.WriteLine("\n UC1StackProblem.");
+            UC1StackProblem st = new UC1StackProblem();
+            st.push(56);
+            st.push(30);
+            st.push(70);
+            st.display();*/
+
+
+            /* Console.WriteLine("\n UC2StackProblem.");
+              UC2StackProblem stackProblem = new UC2StackProblem();
+              stackProblem.push(56);
+              stackProblem.push(30);
+              stackProblem.push(70);
+              stackProblem.peek();
+              stackProblem.display();
+              stackProblem.pop();
+              stackProblem.display();*/
+
+          
+
+            Console.ReadLine();
+        }
+    }
+}
